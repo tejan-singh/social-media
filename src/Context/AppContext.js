be817aa762c8b4e7515e43cb98565e35a2868671
Tejan Singh <tstejan@gmail.com>
2023-06-13 21:15:52 +0530

    feat: show all posts on Home page

--- src/Context/AppContext.js
+++ src/Context/AppContext.js
@@ -1,10 +1,12 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
 
 export const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
   const initialState = {
+    loading: true,
     allPosts: [],
+    errorMsg: "",
   };
 
   const reducerFun = (state, action) => {
@@ -14,14 +16,44 @@ const AppProvider = ({ children }) => {
           ...state,
           allPosts: action.payload,
         };
+      case "SHOW_ERROR":
+        return {
+          ...state,
+          errorMsg: action.payload,
+        };
+      case "HIDE_LOADING":
+        return {
+          ...state,
+          loading: false,
+        };
       default:
         return state;
     }
   };
+  const [ appState, dispatch ] = useReducer(reducerFun, initialState);
+  console.log(appState);
 
-  const { appState, dispatch } = useReducer(reducerFun, initialState);
+  const getPost = async () => {
+    try {
+      const response = await fetch("/api/posts");
+      const { posts } = await response.json();
+      dispatch({ type: "SHOW_ALL_POSTS", payload: posts });
+    } catch (error) {
+      dispatch({ type: "SHOW_ERROR", payload: error.message });
+    } finally {
+      dispatch({ type: "HIDE_LOADING" });
+    }
+  };
 
-  return <AppContext.Provider>{children}</AppContext.Provider>;
+  useEffect(() => {
+    getPost();
+  }, []);
+
+  return (
+    <AppContext.Provider value={{ appState, dispatch }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 export { AppProvider };
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { AppContext } from "../Context/AppContext";
 
 const Home = () => {
+  const {
+    appState: { allPosts, loading, errorMsg },
+  } = useContext(AppContext);
+
+  if (loading) return <p>Loading...</p>;
+  if (errorMsg) return <p>{errorMsg}</p>;
+
   return (
     <div>
-        <h1>Social Media</h1>
+      <h1>Social Media</h1>
+      {allPosts.map(
+        ({
+          _id,
+          id,
+          content,
+          likes: { likeCount, likedBy, dislikedBy },
+          username,
+          createdAt,
+          updatedAt,
+        }) => {
+          return <article key={_id}>
+            <p>{username}</p>
+            <p>{content}</p>
+            <p>{likeCount}</p>
+            {likedBy.map((person) => (
+              <p>{person}</p>
+            ))}
+            {dislikedBy.map((person) => (
+              <p>{person}</p>
+            ))}
+            <p>{createdAt}</p>
+            <p>{updatedAt}</p>
+          </article>;
+        }
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

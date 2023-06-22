655d7405fdf6ab2f46e59da4d71e5bda9593b90f
Tejan Singh <tstejan@gmail.com>
2023-06-22 19:02:57 +0530

    feat: added follow user functionality

--- src/Pages/UserProfile.jsx
+++ src/Pages/UserProfile.jsx
@@ -1,20 +1,26 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import NavBar from "../Components/NavBar";
-import { useParams } from "react-router-dom";
+import {  useParams } from "react-router-dom";
 import { AppContext } from "../Context/AppContext";
 
 const UserProfile = () => {
   const { profileName } = useParams();
+
   const {
-    appState: { allUsers, userProfile },
+    appState: {
+      allUsers,
+      userProfile: {_id, username, firstName, lastName, followers, following },
+      loading,
+    },
     dispatch,
   } = useContext(AppContext);
 
-  const getUserId = async () => {
+  const getUser = async () => {
     try {
       const { _id } = allUsers.find((user) => user.username === profileName);
       const response = await fetch(`/api/users/${_id}`, { method: "GET" });
       const { user } = await response.json();
+      //this will set data in userProfile state which you will get from context and use in jsx
       dispatch({ type: "SET_USER", payload: user });
     } catch (error) {
       console.error(error);
@@ -23,14 +29,37 @@ const UserProfile = () => {
     }
   };
 
+  const handleFollowUser = async () => {
+    try {
+      const response = await fetch(`/api/users/follow/${_id}`, {
+        method: 'POST',
+        headers: {
+          authorization: localStorage.getItem("encodedToken")
+        }
+        
+      })
+      const {followUser} = await response.json() 
+      console.log(followUser)
+      dispatch({ type: "SET_USER", payload: followUser });
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   useEffect(() => {
-    getUserId();
+    getUser();
   }, [allUsers]);
 
+  if (loading) return <p>Loading...</p>;
   return (
     <div>
       <NavBar />
-      {userProfile.username}
+      <p>{`${firstName} ${lastName}`}</p>
+      <p>@{username}</p>
+      <p>Followers: {followers?.length}</p>
+      <p>Following: {following?.length}</p>
+      <button onClick={handleFollowUser}>Follow</button>
     </div>
   );
 };

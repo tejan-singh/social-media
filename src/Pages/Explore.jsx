f248f20c2ae3a9eb469d8b62820252f2e82ca655
Tejan Singh <tstejan@gmail.com>
2023-07-10 02:24:23 +0530

    style: added styles to explore page

--- src/Pages/Explore.jsx
+++ src/Pages/Explore.jsx
@@ -3,21 +3,30 @@ import NavBar from "../Components/NavBar";
 import { AppContext } from "../Context/AppContext";
 import CreatePost from "../Components/CreatePost";
 import Post from "../Components/Post";
+import styles from "./Explore.module.css";
+import Header from "../Components/Header";
 
 const Explore = () => {
   const {
     appState: { allPosts, loading, errorMsg },
-  } = useContext(AppContext)
+  } = useContext(AppContext);
 
   if (loading) return <p>Loading...</p>;
   if (errorMsg) return <p>{errorMsg}</p>;
   return (
-    <div>
-      <NavBar />
-      <CreatePost />
-      {allPosts.map((post) => (
-        <Post {...post} key={post._id} fromHomePage />
-      ))}
+    <div className={styles.layout}>
+      <div className={styles.header}>
+        <Header />
+      </div>
+      <div className={styles.navigation}>
+        <NavBar />
+      </div>
+      <div className={styles.main}>
+        {allPosts.map((post) => (
+          <Post {...post} key={post._id} fromHomePage />
+        ))}
+      </div>
+      <div className={styles.aside}></div>
     </div>
   );
 };

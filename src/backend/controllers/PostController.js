1d376544e99cb2ed89f0445f14e4445f723e673f
Tejan Singh <tstejan@gmail.com>
2023-07-14 18:45:41 +0530

    fix: show full name for new post created

--- src/backend/controllers/PostController.js
+++ src/backend/controllers/PostController.js
@@ -89,6 +89,8 @@ export const createPostHandler = function (schema, request) {
       username: user.username,
       createdAt: formatDate(),
       updatedAt: formatDate(),
+      firstName: user.firstName,
+      lastName: user.lastName,
     };
     this.db.posts.insert(post);
     return new Response(201, {}, { posts: this.db.posts });
@@ -167,7 +169,16 @@ export const likePostHandler = function (schema, request) {
         }
       );
     }
-    const { _id, firstName, lastName, username, createdAt, updatedAt, followers, following } = user;
+    const {
+      _id,
+      firstName,
+      lastName,
+      username,
+      createdAt,
+      updatedAt,
+      followers,
+      following,
+    } = user;
     const postId = request.params.postId;
     const post = schema.posts.findBy({ _id: postId }).attrs;
     if (post.likes.likedBy.some((currUser) => currUser._id === user._id)) {
@@ -181,7 +192,16 @@ export const likePostHandler = function (schema, request) {
       (currUser) => currUser._id !== user._id
     );
     post.likes.likeCount += 1;
-    post.likes.likedBy.push({ _id, firstName, lastName, username, createdAt, updatedAt, followers, following });
+    post.likes.likedBy.push({
+      _id,
+      firstName,
+      lastName,
+      username,
+      createdAt,
+      updatedAt,
+      followers,
+      following,
+    });
     this.db.posts.update({ _id: postId }, { ...post, updatedAt: formatDate() });
     return new Response(201, {}, { posts: this.db.posts });
   } catch (error) {

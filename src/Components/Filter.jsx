ff6a1f70d8c9138381955c2f09b8e73290ac1709
Tejan Singh <tstejan@gmail.com>
2023-07-18 19:15:28 +0530

    style: added styles to filters

--- src/Components/Filter.jsx
+++ src/Components/Filter.jsx
@@ -2,24 +2,54 @@ import React from "react";
 import { useContext } from "react";
 import { AppContext } from "../Context/AppContext";
 import styles from "./Filter.module.css";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFire } from "@fortawesome/free-solid-svg-icons";
+import { faNewspaper } from "@fortawesome/free-regular-svg-icons";
 
 const Filter = () => {
   const { dispatch } = useContext(AppContext);
+  const [activeStyle, setActiveStyle] = useState({
+    latest: true,
+    trending: false,
+  });
 
   const handleLatestPost = () => {
+    setActiveStyle(() => ({ latest: true, trending: false }));
     dispatch({ type: "SORT_BY_LATEST" });
   };
 
   const handleTrendingPost = () => {
+    setActiveStyle(() => ({ trending: true, latest: false }));
     dispatch({ type: "SORT_BY_TRENDING" });
   };
 
   return (
     <div className={styles["filter-container"]}>
-      <p onClick={handleLatestPost} className={styles.filter}>
+      <p
+        onClick={handleLatestPost}
+        className={
+          activeStyle.latest
+            ? `${styles.filter} ${styles.active}`
+            : styles.filter
+        }
+      >
+        <i>
+          <FontAwesomeIcon icon={faNewspaper} />
+        </i>
         Latest
       </p>
-      <p onClick={handleTrendingPost} className={styles.filter}>
+      <p
+        onClick={handleTrendingPost}
+        className={
+          activeStyle.trending
+            ? `${styles.filter} ${styles.active}`
+            : styles.filter
+        }
+      >
+        <i>
+          <FontAwesomeIcon icon={faFire} />
+        </i>
         Trending
       </p>
     </div>
